237e0ac184d6bfaaafbfbe3b288879a455f9fcf8
Matthias Lim <matthias.limeexuan@gmail.com>
2024-02-29 16:49:33 +0800

    Javadoc for Account
    javadoc documentation for Account class, edited gitignore to ignore javadoc folder

--- Account.java
+++ Account.java
@@ -1,3 +1,7 @@
+/**
+ * Represents a bank account associated with a customer.
+ * Each account has an account ID, type, balance, associated customer, and transaction limit.
+ */
 public class Account {
     private int accountId;
     private String accountType;
@@ -5,30 +9,56 @@ public class Account {
     private Customer customer;
     private double transactionLimit;
 
-
+    /**
+     * Constructs a new Account object.
+     */
     public Account(){}
 
-
+    /**
+     * Retrieves the account ID.
+     * @return The account ID.
+     */
     public int getAccountId(){
         return this.accountId;
     }
 
+    /**
+     * Retrieves the current balance of the account.
+     * @return The balance.
+     */
     public double getBalance(){
         return this.balance;
     }
 
+    /**
+     * Retrieves the account type.
+     * @return The account type.
+     */
     public String getAccountType(){
         return this.accountType;
     }
 
+    /**
+     * Retrieves the customer associated with the account.
+     * @return The customer object.
+     */
     public Customer getCustomer(){
         return this.customer;
     }
 
+    /**
+     * Retrieves the transaction limit for the account.
+     * @return The transaction limit.
+     */
     public double getTransactionLimit(){
         return this.transactionLimit;
     }
 
+    /**
+     * Sets the transaction limit for the account.
+     * @param amount The new transaction limit to set.
+     * @return True if the transaction limit was successfully set, false otherwise.
+     */
     public boolean setTransactionLimit(double amount){
         if( amount > 1000){
             return false;
@@ -36,11 +66,20 @@ public class Account {
         this.transactionLimit = amount;
         return true;
     }
-    
+
+    /**
+     * Deposits an amount into the account.
+     * @param amount The amount to deposit.
+     */
     public void deposit(double amount){
         this.balance += amount;
     }
 
+    /**
+     * Withdraws an amount from the account.
+     * @param amount The amount to withdraw.
+     * @return True if the withdrawal was successful, false otherwise.
+     */
     public boolean withdraw(double amount){
         if (this.balance < amount){
             return false;
@@ -48,11 +87,16 @@ public class Account {
         return true;
     }
 
+    /**
+     * Transfers an amount from this account to another account.
+     * @param toAccount The destination account for the transfer.
+     * @param amount The amount to transfer.
+     * @return True if the transfer was successful, false otherwise.
+     */
     public boolean transfer(Account toAccount, double amount){
         if (this.balance < amount){
             return false;
         }
         return true;
     }
-
 }
