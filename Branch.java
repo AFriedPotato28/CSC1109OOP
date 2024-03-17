21b8ee03f8b26c48c50e40e3f36e5716baab4fa0
Shun Hao <teoshunhao@gmail.com>
2024-03-17 11:46:59 +0800

    JavaDoc for branch and currency
    Updated JavaDoc comment for Branch and Currency

--- Branch.java
+++ Branch.java
@@ -1,11 +1,37 @@
+/**
+ * Represent a branch for the bank, including details such as ID, name of the branch, address of the branch
+ * as well as its opening and closing time.
+ */
 public class Branch{
+    /**
+     * The unique identifier of the branch.
+     */
     private int branchId;
+    /**
+     * The name of the branch.
+     */
     private String branchName;
+    /**
+     * The address of the branch.
+     */
     private String branchAddress;
+    /**
+     * The opening hour for the branch.
+     */
     private int openingTime;
+    /**
+     * The closing hour for the branch.
+     */
     private int closingTime;
     
-
+    /**
+     * Construct a Branch object with ID, name address as well as operating hours.
+     * @param branchId The unique identifier of the branch
+     * @param branchName The name of the branch
+     * @param branchAddress The address of the branch
+     * @param openingTime The opening hour for the branch
+     * @param closingTime The closing hour for the branch
+     */
     public Branch(int branchId, String branchName, String branchAddress, int openingTime, int closingTime){
         this.setBranchID(branchId);
         this.setBranchName(branchName);
@@ -14,42 +40,82 @@ public class Branch{
         this.setClosingTime(closingTime);
     }
 
+    /**
+     * Return the Branch ID
+     * @return The branch ID
+     */
     public int getBranchID(){
         return this.branchId;
     }
     
+    /**
+     * Return the Branch Name
+     * @return The branch Name
+     */
     public String getBranchName(){
         return this.branchName;
     }
     
+    /**
+     * Return the Branch Address
+     * @return The branch address
+     */
     public String getBranchAddress(){
         return this.branchAddress;
     }
 
+    /**
+     * Return the Branch Opening Hour
+     * @return The branch opening time
+     */
     public int getOpeningTime(){
         return this.openingTime;
     }
 
+    /**
+     * Return the Branch Closing Hour
+     * @return The branch closing time
+     */
     public int getClosingTime(){
         return this.closingTime;
     }
 
+    /**
+     * Set the Branch ID
+     * @param branchId The branch ID is set
+     */
     public void setBranchID(int branchId){
         this.branchId = branchId;
     }
 
+     /**
+     * Set the Branch Name
+     * @param branchId The branch name is set
+     */
     public void setBranchName(String branchName){
         this.branchName = branchName;
     }
 
+     /**
+     * Set the Branch Address
+     * @param branchId The branch Address is set
+     */
     public void setBranchAddress(String branchAddress){
         this.branchAddress = branchAddress;
     }
 
+     /**
+     * Set the Branch Opening Hour
+     * @param branchId The branch opening hour is set
+     */
     public void setOpeningTime(int openingTime){
         this.openingTime = openingTime; 
     }
 
+     /**
+     * Set the Branch Closing Hour
+     * @param branchId The branch closing time is set
+     */
     public void setClosingTime(int closingTime){
         this.closingTime = closingTime;
     }
--- Currency.java
+++ Currency.java
@@ -1,11 +1,23 @@
+/**
+ * Represent a currency where it include its symbol, purchase price, selling price and the sources for this price.
+ * Also represent currency exchange operation, tracking the cost of buying and selling currencies.
+ */
 public class Currency {
-       
+    
     private String Symbol;
     private double purchasePrice;
     private double sellingPrice;
     private String fromSource;
     private String toSource;
 
+    /**
+     * Construct a new Currency object with symbol, purchase price, selling price, source currency and destination currency
+     * @param symbol The symbol of the currency such as USD, SGD, etc
+     * @param purchasePrice The price at which the currency can be purchased.
+     * @param sellingPrice The price at which the currency can be sold
+     * @param fromSource The source currency for the exchange
+     * @param toSource The destination currency for the exchange
+     */
     public Currency( String symbol, double purchasePrice, double sellingPrice, String fromSource, String toSource) {
         Symbol = symbol;
         this.purchasePrice = purchasePrice;
@@ -14,24 +26,47 @@ public class Currency {
         this.toSource = toSource;
     }
 
+    /**
+     * Construct an empty Currency Object to be used as placeholder before setting specific currency details.
+     */
     public Currency() {}
 
+    /**
+     * Return the symbol of the currency
+     * @return The currency symbol
+     */
     public String getSymbol() {
         return Symbol;
     }
 
+     /**
+     * Return the purchase price of the currency
+     * @return The currency's purchase price
+     */
     public double getpurchasePrice() {
         return purchasePrice;
     }
 
+     /**
+     * Return the selling price of the currency
+     * @return The currency's selling price
+     */
     public double getsellingPrice() {
         return sellingPrice;
     }
   
+     /**
+     * Return the exchange source of the currency 
+     * @return The currency's exchange source
+     */
     public String getFromSource() {
         return fromSource;
     }
 
+     /**
+     * Return the destination source of the currency
+     * @return The currency's destination sourcec
+     */
     public String getToSource() {
         return toSource;
     }
